0011bce3911084786d325238cc56043c23197a46
Jonathan Crozier <jdcrozier@gmail.com>
2021-01-12 21:36:18 +0000

    Add project files.

---
+++ JC.Samples.Processes/Helpers/ProcessHelper.cs
@@ -0,0 +1,113 @@
+﻿using Serilog;
+using System.Diagnostics;
+using System.Linq;
+
+namespace JC.Samples.Processes.Helpers
+{
+    /// <summary>
+    /// Provides methods to help with interacting with processes.
+    /// </summary>
+    public class ProcessHelper
+    {
+        #region Methods
+
+        /// <summary>
+        /// Creates a process that runs in the background with no UI.
+        /// </summary>
+        /// <param name="processName">The name of the process to start</param>
+        /// <param name="processArguments">The command-line arguments to use</param>
+        /// <returns>The created hidden process</returns>
+        public static Process CreateHiddenProcess(string processName, string processArguments)
+        {
+            Log.Information("Creating hidden process '{0}' with arguments '{1}'", processName, processArguments);
+
+            var process                      = new Process();
+            process.StartInfo.WindowStyle    = ProcessWindowStyle.Hidden;
+            process.StartInfo.FileName       = processName;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.Arguments      = processArguments;
+
+            Log.Information("Hidden process '{0}' with arguments '{1}' created", processName, processArguments);
+
+            return process;
+        }
+
+        /// <summary>
+        /// Kills all instances of a specified process.
+        /// </summary>
+        /// <param name="processName">The name of the process to kill</param>
+        /// <param name="timeout">The number of milliseconds to wait for the process to stop</param>
+        public static void KillProcess(string processName, int timeout = 30000)
+        {
+            Log.Information("Checking if any processes named '{0}' exist", processName);
+
+            Process[] processes = Process.GetProcessesByName(processName);
+
+            int processesCount = processes.Count();
+
+            if (processesCount > 0)
+            {
+                Log.Information("Found {0} existing process(es) named '{1}'", processesCount, processName);
+
+                int currentProcessId = Process.GetCurrentProcess().Id;
+
+                foreach (Process process in processes)
+                {
+                    if (process.Id == currentProcessId)
+                    {
+                        Log.Information("Ignoring existing process named '{0}' which matches current process ID of: {1}", processName, currentProcessId);
+                    }
+                    else
+                    {
+                        Log.Information("Killing existing process named '{0}'", processName);
+                        process.Kill();
+                        process.WaitForExit(timeout);
+
+                        if (process.HasExited) Log.Information("Killed existing process named '{0}'", processName);
+                        else Log.Information("Waited for 30 seconds, but couldn't kill process named '{0}'", processName);
+                    }
+                }
+            }
+            else Log.Information("No existing processes named '{0}' exist", processName);
+        }
+
+        /// <summary>
+        /// Checks if a process is currently running.
+        /// </summary>
+        /// <param name="processName">The name of the process to check</param>
+        /// <returns>True if the process is running, otherwise false</returns>
+        public static bool ProcessIsRunning(string processName)
+        {
+            Log.Information("Checking if any processes named '{0}' exist", processName);
+            
+            Process[] processes = Process.GetProcessesByName(processName);
+
+            int processesCount = processes.Count();
+
+            if (processesCount > 0)
+            {
+                Log.Information("Found {0} existing process(es) named '{1}'", processesCount, processName);
+
+                int currentProcessId = Process.GetCurrentProcess().Id;
+
+                foreach (Process process in processes)
+                {
+                    if (process.Id == currentProcessId)
+                    {
+                        Log.Information("Ignoring existing process named '{0}' which matches current process ID of: {1}", processName, currentProcessId);
+                    }
+                    else
+                    {
+                        Log.Information("Found existing process named '{0}'", processName);
+                        return true;
+                    }
+                }
+            }
+            else Log.Information("No existing processes named '{0}' exist", processName);
+
+            return false;
+        }
+
+        #endregion
+    }
+}
---
+++ JC.Samples.Processes/Program.cs
@@ -0,0 +1,72 @@
+﻿using JC.Samples.Processes.Helpers;
+using Serilog;
+using System;
+using System.Diagnostics;
+
+namespace JC.Samples.Processes
+{
+    /// <summary>
+    /// Main Program class.
+    /// </summary>
+    class Program
+    {
+        #region Methods
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// <param name="args">The command-line arguments</param>
+        static void Main(string[] args)
+        {
+            // Configure Serilog.
+            Log.Logger = new LoggerConfiguration()
+               .MinimumLevel.Information()
+               .WriteTo.Console()
+               .CreateLogger();
+
+            // Output display heading (Create Hidden Process).
+            // ===============================================
+            string hiddenProcessOutputMessage = "Create Hidden Process";
+
+            Console.WriteLine(hiddenProcessOutputMessage);
+            Console.WriteLine("".PadLeft(hiddenProcessOutputMessage.Length, '='));
+            Console.WriteLine();
+
+            // Create a hidden process and start it.
+            Process process = ProcessHelper.CreateHiddenProcess("notepad", @"C:\Windows\System32\drivers\etc\hosts");
+            process.Start();
+
+            // Output display heading (Processing Is Running).
+            // ===============================================
+            string isRunningOutputMessage = "Process Is Running";
+
+            Console.WriteLine();
+            Console.WriteLine(isRunningOutputMessage);
+            Console.WriteLine("".PadLeft(isRunningOutputMessage.Length, '='));
+            Console.WriteLine();
+
+            // Check if a process is running.
+            bool isRunning = ProcessHelper.ProcessIsRunning("notepad");
+
+            // Output display heading (Kill Process).
+            // ======================================
+            string killProcessOutputMessage = "Kill Process";
+
+            Console.WriteLine();
+            Console.WriteLine(killProcessOutputMessage);
+            Console.WriteLine("".PadLeft(killProcessOutputMessage.Length, '='));
+            Console.WriteLine();
+
+            // Stop a process.
+            ProcessHelper.KillProcess("notepad");
+
+            // Inform the user that the program has completed.
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit");
+
+            Console.ReadKey();
+        }
+
+        #endregion
+    }
+}
